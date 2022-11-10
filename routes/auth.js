7dd524d66f8c7f18d0f7a8a8a484942e125fa2c2
emazou <estefania.mazou@gmail.com>
2022-11-10 00:44:45 -0500

    route added

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
 
-const { signUp, verifyMail, signOut } = require('../controllers/userController');
-
+const { signUp, verifyMail, signOut, signIn } = require('../controllers/userController');
 
 router.route('/sign/up')
     .post(signUp)
 
+router.route('/sign/in')
+    .post(signIn)
+
 router.route('/sign/out')
-    .post(signOut)
-    
+    .post(signOut) 
+
 router.route('/verify/:code')
     .get(verifyMail)
-
 
 router.route('/:id')
     .get()
