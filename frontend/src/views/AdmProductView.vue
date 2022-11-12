ec02e80e611e7884b7faf971ff619e777f06afcd
emazou <estefania.mazou@gmail.com>
2022-11-12 17:35:22 -0500

    view created

---
+++ frontend/src/views/AdmProductView.vue
@@ -0,0 +1,30 @@
+<template>
+    <HeaderBar :newProduct='true' />
+    <div class='container'>
+        <FormProduct />
+    </div>
+</template>
+<script>
+import FormProduct from '../components/FormProduct.vue';
+import HeaderBar from '../components/HeaderBar.vue';
+export default {
+    props: ["id"],
+    data() {
+    },
+    components: { 
+        FormProduct,
+        HeaderBar 
+    }
+}
+</script>
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    width: 100%;
+    background-color: #1e1e1e0b;
+    min-height: 90vh;
+}
+</style>
