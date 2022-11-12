c59f451ed89e6ea22251f0bcf15ac5a3f62f574d
emazou <estefania.mazou@gmail.com>
2022-11-12 11:54:05 -0500

    Date deleted

--- models/Product.js
+++ models/Product.js
@@ -27,10 +27,6 @@ const productSchema = new mongoose.Schema({
         min: 1,
         max: 5,
     },
-    date: {
-        type: Date,
-        required: true,
-    },
     user: {
         type: mongoose.Types.ObjectId,
         ref: 'users',
