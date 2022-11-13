5d5e7c2ea12ea55cb84da0572c614b5d8af78d10
emazou <estefania.mazou@gmail.com>
2022-11-13 12:40:59 -0500

    fileupload deleted

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ require('dotenv').config();
 require('./config/database');
 const cors = require('cors');
 const express = require('express');
-const fileUpload = require('express-fileupload');
 const indexRouter = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
-app.use(fileUpload());
 app.use(express.json());
 
 indexRouter(app);
